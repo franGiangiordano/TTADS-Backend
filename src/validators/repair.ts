0c1dac4fc967237623e3929e27b9d2cf3705757e
franGiangiordano <frangiangiordano@gmail.com>
2023-11-16 12:50:57 -0300

    validator schema repair

--- src/validators/repair.ts
+++ src/validators/repair.ts
@@ -1,5 +1,5 @@
-import { z } from "zod";
 import { Request, Response, NextFunction } from "express";
+import { repairSchema } from "./schemas/repair";
 
 const validatorRepair: ((
   req: Request,
@@ -11,33 +11,12 @@ const validatorRepair: ((
       req.body = {
         description: req.body.description,
         cost: req.body.cost,
-        date: req.body.date,
         equipment: req.body.equipment,
       };
 
       const isPutRequest = req.method === "PUT";
 
-      const description = z
-        .string()
-        .nonempty({ message: "La descripción no puede estar vacía" });
-
-      const cost = z
-        .number()
-        .positive({ message: "El costo debe ser un número positivo" });
-
-      const date = z.date();
-
-      const equipment = z.object({
-        type: z.string(),
-        id: z.string(),
-      });
-
-      const schema = z.object({
-        description: isPutRequest ? description.optional() : description,
-        cost: isPutRequest ? cost.optional() : cost,
-        date: isPutRequest ? date.optional() : date,
-        equipment: isPutRequest ? equipment.optional() : equipment,
-      });
+      const schema = isPutRequest ? repairSchema.optional() : repairSchema;
 
       const validatedData = schema.safeParse(req.body);
 
@@ -55,5 +34,3 @@ const validatorRepair: ((
 ];
 
 export default validatorRepair;
-
-
---
+++ src/validators/schemas/repair.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import { equipmentSchema } from "./equipment";
+
+const description = z
+  .string()
+  .nonempty({ message: "La descripción no puede estar vacía" });
+
+const cost = z
+  .number()
+  .positive({ message: "El costo debe ser un número positivo" });
+
+export const repairSchema = z.object({
+  description: description,
+  cost: cost,
+  equipment: equipmentSchema,
+});
