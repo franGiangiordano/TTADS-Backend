f88d68a65a1bb3efc86c0054622d619c60519462
franGiangiordano <frangiangiordano@gmail.com>
2023-11-18 17:34:34 -0300

    Update driver.ts

--- src/routes/driver.ts
+++ src/routes/driver.ts
@@ -13,7 +13,6 @@ import { verifyToken, isManager, isAdmin } from "../middlewares/authJwt.js";
 const router = express.Router();
 
 router.get("/", getDrivers);
-router.get("/count", getDrivers);
 router.get("/:id", getDriver);
 router.post("/", [verifyToken, isAdmin], validatorDriver, createDriver);
 router.put("/:id", [verifyToken, isAdmin], validatorDriver, updateDriver);
