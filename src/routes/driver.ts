106e0ef9f9c12f19c93d634004332eb9acf77f62
franGiangiordano <frangiangiordano@gmail.com>
2023-11-18 14:54:12 -0300

    update driver.ts routs (cards dashboard)

--- src/routes/driver.ts
+++ src/routes/driver.ts
@@ -13,6 +13,7 @@ import { verifyToken, isManager, isAdmin } from "../middlewares/authJwt.js";
 const router = express.Router();
 
 router.get("/", getDrivers);
+router.get("/count", getDrivers);
 router.get("/:id", getDriver);
 router.post("/", [verifyToken, isAdmin], validatorDriver, createDriver);
 router.put("/:id", [verifyToken, isAdmin], validatorDriver, updateDriver);
