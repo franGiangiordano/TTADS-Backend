11879fe2434907d3fcab6a6e5b3173fcd37d11a3
gturconi <35380017+gturconi@users.noreply.github.com>
2024-02-27 15:50:00 -0300

    Update batea.spec.ts

--- tests/batea.spec.ts
+++ tests/batea.spec.ts
@@ -2,23 +2,25 @@ import { EntityListResponse } from "../src/models/entity.list.response.model";
 
 const { getBateas } = require("../src/controllers/batea");
 
-const Batea = require('../src/models/batea');
+const Batea = require("../src/models/batea");
 
-const bateasMock = [{
-  patente: "AAA"
-}];
+const bateasMock = [
+  {
+    patent: "AAA",
+  },
+];
 
-jest.mock('../src/models/batea', () => ({
+jest.mock("../src/models/batea", () => ({
   countDocuments: jest.fn(),
   find: jest.fn(),
 }));
 
-describe('getBateas', () => {
+describe("getBateas", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  it('should get bateas with default parameters', async () => {
+  it("should get bateas with default parameters", async () => {
     const req = { query: {} };
     const res = { json: jest.fn() };
     const bateasCount = 20;
@@ -26,11 +28,13 @@ describe('getBateas', () => {
     Batea.countDocuments.mockResolvedValueOnce(bateasCount);
     Batea.find.mockReturnValueOnce({
       skip: jest.fn().mockReturnThis(),
-      limit: jest.fn().mockResolvedValue(bateasMock)
+      limit: jest.fn().mockResolvedValue(bateasMock),
     });
 
     await getBateas(req, res);
 
-    expect(res.json).toHaveBeenCalledWith(new EntityListResponse(bateasMock, bateasCount, 0, 2));
+    expect(res.json).toHaveBeenCalledWith(
+      new EntityListResponse(bateasMock, bateasCount, 0, 2)
+    );
   });
 });
