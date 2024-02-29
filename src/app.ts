67ab445ad4b4798c18f27ed88370a27b85db99ad
Martin Oscar Lambrecht <martinoscarlambrecht@gmail.com>
2024-02-29 19:15:02 -0300

    Cors allowAllOrigin

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 app.use("/api/", routes);
 
